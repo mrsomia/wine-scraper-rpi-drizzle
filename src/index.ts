0b19863fb6621bcc23fa6b36c947f44b8b54346d
Sachin Somia <sachinsomia@gmail.com>
2023-04-02 16:25:07 +0100

    Feat: Moves to just a script that runs and exits

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,15 @@
-import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
-import schedule from 'node-schedule'
-import { db } from './lib/db.js';
-import { scrapePricesAndAddToDB } from './lib/scrape-utils.js';
-import { makeMessageArray, pingDetails } from './lib/notification.js'
-import { getLatestPrices } from './routes/item-prices.js';
-import { addNewItem } from './routes/item.js';
+import { db } from "./lib/db.js";
+import { scrapePricesAndAddToDB } from "./lib/scrape-utils.js";
+import { makeMessageArray, pingDetails } from "./lib/notification.js";
 
-export const fastify = Fastify({
-  logger: {
-    level : 'info',
-    file: './logs/logs.txt'
-  }
-})
-
-const job = schedule.scheduleJob('0 14 * * *', async function(){
-  await scrapePricesAndAddToDB(db)
-  let messageArr = makeMessageArray(db)
+async function main() {
+  await scrapePricesAndAddToDB(db);
+  let messageArr = makeMessageArray(db);
   try {
-    pingDetails(messageArr)
+    pingDetails(messageArr);
   } catch (err) {
-    fastify.log.error(err)
+    console.error(err);
   }
-})
+}
 
-
-fastify.get('/item-prices', getLatestPrices)
-fastify.post('/item', addNewItem)
-
-fastify.listen(8080, "127.0.0.1", function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  console.log(`Server is now listening on ${address}`)
-})
+main();
